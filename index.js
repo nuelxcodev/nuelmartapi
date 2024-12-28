21b289f1a8b2a0459d039ff3fa83722e72fe4686
chukwuemeka <chukwuemekacodev@gmail.com>
2024-12-28 14:34:28 +0100

    cors update

--- index.js
+++ index.js
@@ -11,13 +11,18 @@ const PORT = process.env.PORT;
 
 app.use("/api", router);
 app.use(express.json());
-app.use(cors({ origin: "https://nuelmart.netlify.app" })); // Update this with your frontend URL
+app.use(
+  cors({
+    origin: "https://nuelmart.netlify.app",
+    methods: ["GET", "POST", "PUT", "DELETE"], 
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+); // Update this with your frontend URL
 app.use(cookieParser());
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.url}`);
   next();
 });
-
 
 // Wrap the async Dbconnection call and server start in an async function
 async function startServer() {
