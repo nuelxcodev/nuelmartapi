93c36d691479550f5b295bbaa2691825a52a26c0
chukwuemeka <chukwuemekacodev@gmail.com>
2024-12-28 11:52:37 +0100

    create router

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const {Router} = require('express')
 const app = express();
 require("dotenv").config();
 
 const PORT = process.env.PORT
+
+app.use('/api',)
+
 
 app.listen(PORT, ()=>{
     console.log(`server is now runing on PORT ${PORT}`)
