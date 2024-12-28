343f6791414da38f72963678868c7181ce358c6e
nuelxcodev <144784983+nuelxcodev@users.noreply.github.com>
2024-12-28 14:16:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const { Router } = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 const app = express();
 require("dotenv").config();
 const router = require("./route/approutes.js");
@@ -8,6 +10,14 @@ const Dbconnection = require("./utils/mongodbcon.js");
 const PORT = process.env.PORT;
 
 app.use("/api", router);
+app.use(express.json());
+app.use(cors({ origin: "https://nuelmart.netlify.app" })); // Update this with your frontend URL
+app.use(cookieParser());
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
 
 // Wrap the async Dbconnection call and server start in an async function
 async function startServer() {
